283a08b846d693dafa47307b8ff4d4798dc81840
Gohil Gnandipainh <abhijitsinh438@gmail.com>
2026-02-06 14:05:49 +0530

    Add files via upload

---
+++ c language/1_intro/1_basic_stuct.c/index.c
@@ -0,0 +1,10 @@
+
+#include<stdio.h>
+
+int main()
+{
+    printf("welcome to c language");
+    
+    return 0;
+}
+ 
---
+++ c language/1_intro/2_escape_sequence.c/index.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("good morning \n all student");
+
+    printf("\n gnandipsinh \n");
+
+    // space 
+
+    printf("hi\t\t hello");
+
+    printf("\n slash\\");
+
+    // coma
+
+
+    printf("\ncoma\"");
+
+    return 0;
+
+
+}
