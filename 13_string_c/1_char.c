e704fa129692e2f1745190551f422d73afcc5bcf
Gohil Gnandipainh <abhijitsinh438@gmail.com>
2026-03-17 16:29:25 +0530

    Add files via upload

---
+++ 13_string_c/1_char.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    char letter = 'a';
+    printf("%c\n", letter);
+
+    printf("%d\n", letter);
+
+    char word = 'z';
+    printf("%c\n", word);
+
+    printf("%d\n", word);
+
+    char fristletter = 'A';
+    printf("%c\n", fristletter);
+
+    printf("%d\n", fristletter);
+
+    char lastletter = 'Z';
+    printf("%c\n", lastletter);
+
+    printf("%d\n", lastletter);
+
+
+
+    return 0;
+}
